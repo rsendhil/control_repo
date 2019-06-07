a2fdba8010b0b5a36c5a2ac775d2fe247a5f24ec
testofwill22 <35222040+testofwill22@users.noreply.github.com>
2019-06-07 22:52:01 +0100

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,9 @@
 class profile::agent_nodes {
   include dockeragent
-  dockeragent::node {'web.puppet.vm':}
-  dockeragent::node {'db.puppet.vm':}
+  dockeragent::node {'web.puppet.vm':
+    ports => ["10080:80"],
+  }
+  dockeragent::node {'db.puppet.vm':
+    ports => ["20080:80"],
+  }
 }
