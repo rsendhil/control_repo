6c58fefba6ad1152ec6c403b4e7c74e8c742db65
testofwill22 <35222040+testofwill22@users.noreply.github.com>
2019-06-08 00:06:03 +0100

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,11 @@ class profile::base {
   user { 'root':
     ensure => present,
   }
+  
+  yumrepo { 'docker-ce-stable':
+    descr    => 'Docker CE Stable - $basearch',
+    baseurl  => 'https://download.docker.com/linux/fedora/28/$basearch/stable',
+    gpgkey   => 'https://download.docker.com/linux/fedora/gpg',
+    gpgcheck => true,
+  }
 }
