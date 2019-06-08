fc28bb1ea402e2cb6dbd1d1705f140578895a6af
testofwill22 <35222040+testofwill22@users.noreply.github.com>
2019-06-08 23:43:46 +0100

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -5,7 +5,7 @@ class profile::base {
   
   yumrepo { 'docker-ce-stable':
     descr    => 'Docker CE Stable - $basearch',
-    baseurl  => 'https://download.docker.com/linux/centos/7/$basearch/stable'
+    baseurl  => 'https://download.docker.com/linux/centos/7/$basearch/stable',
     gpgkey   => 'https://download.docker.com/linux/centos/gpg',
     gpgcheck => true,
   }
